605f5a3ac79c4360ba58f6d8c6fd9998aa04a3e0
Sommyj1 <joseph2blessing2015@gmail.com>
2023-03-22 10:08:17 -0700

    Add documentation for __str__

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Square module
+"""
+
+
+class Square():
+    """
+    Represent a square
+    """
+
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.height * self.width
+
+    def permiter_of_my_square(self):
+        """ Perimiter of the square """
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """ String representation of Square """
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.permiter_of_my_square())
